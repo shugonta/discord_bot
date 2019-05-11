5490bef5057255f4d0cfbf03a80e0d095e173783
shugonta <shugonta@outlook.com>
2019-05-11 22:55:53 +0900

    herokuファイル

--- voice_channel_status.py
+++ voice_channel_status.py
@@ -1,19 +1,20 @@
 import datetime
+import pytz
 
 
 class VoiceChannelStatus:
     def __init__(self):
-        self.last_changed_time = datetime.datetime.now()
+        self.last_changed_time = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
         self.last_post_time = None
         self.last_invite_time = None
 
     def status_change(self):
-        self.last_changed_time = datetime.datetime.now()
+        self.last_changed_time = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
         self.last_post_time = None
         self.last_invite_time = None
 
     def post(self):
-        self.last_post_time = datetime.datetime.now()
+        self.last_post_time = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
 
     def invite(self):
-        self.last_invite_time = datetime.datetime.now()
+        self.last_invite_time = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
