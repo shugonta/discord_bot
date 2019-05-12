c70e6edd0ea9e03fce0943a9b33a71607970175a
shugonta <shugonta@outlook.com>
2019-05-12 13:35:40 +0900

    herokuファイル

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
+import os
+
+
 class Config():
-    token = 'NTc2NjcxOTAxNTk5MDA2NzMx.XNZ-aA.X5tFOz-l7uCA0TDKTuEHnMHGA20'
-    guild = 345804296991801344
+    token = os.environ["DISCORD_TOKEN"]
+    guild = os.environ["DISCORD_GUILD"]
     general = 345804296991801345
     off_sanka = 389011203206283264
     aeon_cinema = 345804296991801346
     pyongyang = 404983177669771264
-    bot_id = 576671901599006731
+    bot_id = os.environ["BOT_ID"]
     timeout_time = 600
     post_interval = 1200
     invite_interval = 1200
     status_check_period = 10
     delete_period = 3600
+
+    line_access_token = os.environ["LINE_TOKEN"]
