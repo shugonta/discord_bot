0a56009b91225a587c27653adba6c5dffdfd71ee
shugonta <shugonta@outlook.com>
2019-05-12 15:12:02 +0900

    Line対応

--- flask_server.py
+++ flask_server.py
@@ -69,12 +69,6 @@ def message_text(event):
     )
 
 
-if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
-
-    app.run(debug=options.debug, port=options.port)
+def app_start():
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, threaded=True)
