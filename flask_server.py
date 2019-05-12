fe572d8fd61a2f1c429e31be5544e63efcc969b3
shugonta <shugonta@outlook.com>
2019-05-12 15:32:59 +0900

    サーバテスト

--- flask_server.py
+++ flask_server.py
@@ -70,8 +70,9 @@ def message_text(event):
 
 
 def app_start():
-    # port = int(os.getenv("PORT", 5000))
-    app.run(threaded=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
 if __name__ == "__main__":
     app_start()
