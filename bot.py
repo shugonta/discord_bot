856ccdfa9372ba4321638744b1b8f1abae3704ed
shugonta <shugonta@outlook.com>
2020-08-15 00:18:51 +0900

    app_id -> app_name

--- bot.py
+++ bot.py
@@ -42,19 +42,19 @@ async def check_channel(guild_id):
         await asyncio.sleep(conf.status_check_period)
 
         guild = client.get_guild(guild_id)
-        for guild_member in guild.members:
-            if guild_member.id not in last_app_id:
-                last_app_id[guild_member.id] = None
-            if guild_member.activity is None and last_app_id[guild_member.id] is not None:
-                print("Member %s exited app_id %s" % (guild_member.id, last_app_id[guild_member.id]))
-                if last_app_id[guild_member.id] is not None:
-                    last_app_id[guild_member.id] = None
-            elif guild_member.activity is not None and last_app_id[guild_member.id] is None:
-                print("Member %s started app_id %s" % (guild_member.id, guild_member.activity.name))
-                if last_app_id[guild_member.id] != guild_member.activity.name:
-                    messages = TextSendMessage(text="おい、%s！。お前さっき俺が着替えてる時、チラチラ%sやってただろ" % (guild_member.name, guild_member.activity.name))
-                    line_bot_api.push_message(conf.line_group, messages=messages)
-                    last_app_id[guild_member.id] = guild_member.activity.name
+        # for guild_member in guild.members:
+        #     if guild_member.id not in last_app_id:
+        #         last_app_id[guild_member.id] = None
+        #     if guild_member.activity is None and last_app_id[guild_member.id] is not None:
+        #         print("Member %s exited app_id %s" % (guild_member.id, last_app_id[guild_member.id]))
+        #         if last_app_id[guild_member.id] is not None:
+        #             last_app_id[guild_member.id] = None
+        #     elif guild_member.activity is not None and last_app_id[guild_member.id] is None:
+        #         print("Member %s started app_id %s" % (guild_member.id, guild_member.activity.name))
+        #         if last_app_id[guild_member.id] != guild_member.activity.name:
+        #             messages = TextSendMessage(text="おい、%s！。お前さっき俺が着替えてる時、チラチラ%sやってただろ" % (guild_member.name, guild_member.activity.name))
+        #             line_bot_api.push_message(conf.line_group, messages=messages)
+        #             last_app_id[guild_member.id] = guild_member.activity.name
 
         for id, voice_channel in voice_channel_list.items():
             channel = client.get_channel(id)
